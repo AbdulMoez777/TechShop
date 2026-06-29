3f9558035731f1d54076972dd12766c95fc74e4f
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-06-29 20:56:36 +0500

    feat: Headr & navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ProductDetail from "./pages/ProductDetail";
+import Footer from "./components/Footer";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
 
-export default App
+      <Router>
+        <div className="min-h-screen bg-gray-500 text-white font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+
+    </>
+  );
+};
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { House,MonitorSmartphone, ShoppingCart} from "lucide-react";
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <header className="sticky top-0 bg-gray-950/95 backdrop-blur-md text-white shadow-2xl shadow-gray-950/70 border-b border-orange-900">
+        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+          <Link to={"/"}>
+            <div className="flex items-center space-x-3 cursor-pointer">
+              <House className="w-8 h-8  text-orange-400 drop-shadow-lg" />
+              <h1 className="flex gap-2 text-4xl font-extrabold items-center justify-center">
+                TechShop{" "}
+                <span className="w-5 h-5">
+                  <MonitorSmartphone />
+                </span>
+              </h1>
+            </div>
+          </Link>
+          <nav className="flex items-center space-x-6">
+            <Link to={"/Cart"}
+            className="relative p-3 bg-orange-500/10 rounded-xl hover:bg-orange-500/20 transit:duration-300  border-orange-400/50 shadow-lg cursor-pointer">
+             <ShoppingCart className="w-6 h-6 text-orange-400" />
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
