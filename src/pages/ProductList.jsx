9f6b883962f5a465aeb401604aca9db4752f4ab9
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-04 04:48:56 +0500

    feat: UI CategoryFilter

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchFilter from "../components/SearchFilter";
+import CategoryFilter from "../components/CategoryFilter";
 
 function ProductList() {
   return (
     <>
       <div className="continer mx-auto px-4 md:px-8 pt-8">
         <SearchFilter />
+        <CategoryFilter />
       </div>
     </>
   );
