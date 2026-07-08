7a60fbde092bfccbd32563ef0f5098d16c4bb5df
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-09 03:37:21 +0500

    feat: Logic for Add Items in Cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,14 +1,39 @@
-import { createContext, use, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
-import {initialProducts} from "../data/product"
+import { initialProducts } from "../data/product";
 
 export const CartProvider = (props) => {
-    const products = initialProducts;
+  const products = initialProducts;
 
-    return (
-        <CartContext.Provider value={{ products }}> {props.children} </CartContext.Provider>
-    );
+  const [cart, setCart] = uCeState([]);
+
+  //   Add item into the Cart
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  
+
+
+
+
+  return (
+    <CartContext.Provider value={{ products }}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export const useCart = () => useContext(CartContext);
