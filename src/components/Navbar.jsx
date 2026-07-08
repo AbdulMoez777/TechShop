a16f01e52b7dcde7cc68bb647e17735e4a14d46e
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-09 04:30:13 +0500

    feat: Using Cart functions in NavBar, ProductDetail, ProductCard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { House,MonitorSmartphone, ShoppingCart} from "lucide-react";
 import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 
 function Navbar() {
+  const { cartCount } = useCart();
+
   return (
     <>
       <header className="sticky top-0 bg-gray-950/95 backdrop-blur-md text-white shadow-2xl shadow-gray-950/70 border-b border-orange-900">
@@ -19,10 +22,16 @@ function Navbar() {
               </h1>
             </div>
           </Link>
+
           <nav className="flex items-center space-x-6">
             <Link to={"/Cart"}
             className="relative p-3 bg-orange-500/10 rounded-xl hover:bg-orange-500/20 transit:duration-300  border-orange-400/50 shadow-lg cursor-pointer">
              <ShoppingCart className="w-6 h-6 text-orange-400" />
+              {cartCount > 0 && (
+                <span className="absolute -top-1 -right-1 bg-orange-500 text-white text-xs font-bold px-2 py-1 rounded-full">
+                  {cartCount}
+                </span>
+              )}
             </Link>
           </nav>
         </div>
